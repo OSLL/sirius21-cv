113212c36bae7def39338ee2cd4823c7d7f5b65f
Arseniy <yarmolinskiyam@gmail.com>
2022-01-12 13:12:19 +0300

    Обработка пользовательского ввода

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import sys
+
 def main():
-    print("Hello World!")
+    if len(sys.argv) == 1:
+        print("Invalid arguments.\n\nType -h or --help to get help")
+        return
+    if sys.argv[1] == '--help' or sys.argv[1] == '-h':
+        print("Use flag -p or --path to specify path to file")
+        return
+    elif sys.argv[1] == '--path' or sys.argv[1] == '-p':
+        path = sys.argv[2]
+    else:
+        print("Invalid arguments.\n\nType -h or --help to get help")
+        return
+
+    print(path)
+
 
 if __name__ == "__main__":
     main()
