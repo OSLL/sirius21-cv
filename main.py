1e6aa22128f4f6c6dd08a512d863cfca39a71030
YarmMV <53021283+YarmMarita@users.noreply.github.com>
2022-01-14 23:17:21 +0300

    Решение задачи обработка пользовательского ввода

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import sys
+
 def main():
-    print("Hello World!")
+    path = ''
+
+    # Начало кода решения
+    if(len(sys.argv)<=1):
+        print('incorrect input')
+        print('Type -h or --help to get help')
+        exit()
+
+    if (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
+        print('Use flag -p or --path to specify path to file')
+        exit()
+    elif (sys.argv[1] == '-p' or sys.argv[1] == '--path'):
+        path=sys.argv[2]
+    else:
+        print('Invalid arguments.')
+        print('Type -h or --help to get help')
+        exit()
+    # Конец кода решения
+
+    print(path)
+
 
 if __name__ == "__main__":
     main()
