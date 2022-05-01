79f00a11fc2f7befbad9cdb62ce0d8ceb9ebb16b
YarmMV <yarmolinskaya@mail.ru>
2022-05-01 23:35:51 +0300

    Решение для задачи вводного занятия

--- 1. Вводное занятие/main.py
+++ 1. Вводное занятие/main.py
@@ -1,5 +1,19 @@
 def main():
+    import cv2 as cv
+    import os.path
+
     print("Hello World!")
+    path_file = input("Bведите путь к файлу:")
+    if os.path.exists(path_file):
+        img = cv.imread(path_file, 1)
+        mask_low, mask_hi = (55, 100, 100), (255, 255, 255)
+        img_mask = cv.inRange(img, mask_low, mask_hi)
+        cv.imshow("win1", img)
+        cv.imshow("win2", img_mask)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+    else:
+        print("Такой файл не существует")
 
 if __name__ == "__main__":
     main()
