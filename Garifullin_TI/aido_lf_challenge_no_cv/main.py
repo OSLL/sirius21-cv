83a3d8a4d78ac823422e68f925ff22f9869efe7c
Murkat-git <murkat2007@yandex.ru>
2022-05-13 18:26:37 +0500

    aido_lf_challenge_no_cv

--- Garifullin_TI/aido_lf_challenge_no_cv/main.py
+++ Garifullin_TI/aido_lf_challenge_no_cv/main.py
@@ -13,16 +13,11 @@ class LfChallengeNoCvTaskSolution(TaskSolution):
             lane_pose = env.get_lane_pos2(env.cur_pos, env.cur_angle)
             distance_to_road_center = lane_pose.dist
             angle_from_straight_in_rads = lane_pose.angle_rad
-            print(f'''Dist: {distance_to_road_center}
-Angle: {angle_from_straight_in_rads}
-----------------------''')
             # Требуется по положению робота в полосе определить линейную и угловые скорости
-            speed = 0.2
-            if abs(angle_from_straight_in_rads) < 0.03:
-                speed = 0.5
-            if angle_from_straight_in_rads > 0:
-                steering = 1
-            else:
-                steering = -1
+            print("distance = ", distance_to_road_center, " angle = ", angle_from_straight_in_rads)
+            steering = distance_to_road_center * 25 + angle_from_straight_in_rads * 15
+            speed = 1 - abs(steering) * 0.35
+            if speed < 0:
+                speed = 0
             env.step([speed, steering])
             env.render()
